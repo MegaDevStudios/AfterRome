274243a355dc39f23435fd990d9a39d9f36a6f3b
Dreaght <nikitaefser20092007@gmail.com>
2024-05-22 21:07:58 +0300

    "prog" command added

---
+++ src/main/java/com/megadev/afterrome/command/ProgCommand.java
@@ -0,0 +1,12 @@
+package com.megadev.afterrome.command;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import org.bukkit.entity.Player;
+
+public class ProgCommand extends BaseCommand {
+    @CommandAlias("progress|prog")
+    public void onCommand(Player player) {
+
+    }
+}
