719c4a47c6ce845d6a2fa2e8a9ad57ec4a627c0f
Legeu <maksprostoved21@gmail.com>
2024-05-29 18:34:44 +0400

    added execute() at skill

--- src/main/java/com/megadev/afterrome/object/menu/shop/upgrade/skill/Skill.java
+++ src/main/java/com/megadev/afterrome/object/menu/shop/upgrade/skill/Skill.java
@@ -1,7 +1,10 @@
 package com.megadev.afterrome.object.menu.shop.upgrade.skill;
 
 import com.megadev.afterrome.object.menu.item.MenuItem;
+import org.bukkit.event.Event;
 
 public interface Skill {
     MenuItem getMenuItem();
+
+    void execute(Event event);
 }
