5308df9151969548ac33c8ca48874648abea06f2
dreaght <nikitaefser20092007@gmail.com>
2024-05-31 23:46:37 +0300

    TreeCapitator fix

--- src/main/java/com/megadev/afterrome/util/TreeCapitator.java
+++ src/main/java/com/megadev/afterrome/util/TreeCapitator.java
@@ -3,13 +3,9 @@ package com.megadev.afterrome.util;
 import dev.mega.megacore.MegaCore;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.Explosive;
-import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.BlockIterator;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TreeCapitator {
     private final Location location;
@@ -21,25 +17,50 @@ public class TreeCapitator {
     }
 
     public void treeCapitate() {
+        Queue<Location> woodLocations = new LinkedList<>();
+        Set<Location> processedLocations = new HashSet<>();
+
+        if (hasMaterial(location)) {
+            woodLocations.add(location);
+        }
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (woodLocations.isEmpty()) {
+                    cancel();
+                    return;
+                }
+
+                Location currentLocation = woodLocations.poll();
+                if (currentLocation != null) {
+                    currentLocation.getBlock().setType(Material.AIR);
+                    processedLocations.add(currentLocation);
+
+                    for (Location neighbor : getNeighboringLocations(currentLocation)) {
+                        if (hasMaterial(neighbor) && !processedLocations.contains(neighbor)) {
+                            woodLocations.add(neighbor);
+                            processedLocations.add(neighbor);
+                        }
+                    }
+                }
+            }
+        }.runTaskTimer(MegaCore.getInstance(), 1, 1);
+    }
+
+    private List<Location> getNeighboringLocations(Location location) {
         List<Location> neighboringLocations = new ArrayList<>();
 
-        for (int x = 0; x < 2; x++) {
-            for (int y = 0; y < 2; y++) {
-                for (int z = 0; z < 2; z++) {
-                    neighboringLocations.add(location.clone().add(-1 + x, -1 + y, -1 + z));
+        for (int x = -1; x <= 1; x++) {
+            for (int y = -1; y <= 1; y++) {
+                for (int z = -1; z <= 1; z++) {
+                    if (x == 0 && y == 0 && z == 0) continue;
+                    neighboringLocations.add(location.clone().add(x, y, z));
                 }
             }
         }
 
-         neighboringLocations.stream()
-                .filter(this::hasMaterial)
-                .forEach(loc -> new BukkitRunnable() {
-            @Override
-            public void run() {
-                loc.getWorld().getBlockAt(location).setType(Material.STONE);
-                new TreeCapitator(loc, material).treeCapitate();
-            }
-        }.runTask(MegaCore.getInstance()));
+        return neighboringLocations;
     }
 
     private boolean hasMaterial(Location location) {
