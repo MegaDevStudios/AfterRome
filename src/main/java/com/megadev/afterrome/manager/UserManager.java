d94674115fcecf9f5d49e98ecf6a46de084ff408
Legeu <maksprostoved21@gmail.com>
2024-06-08 22:02:34 +0400

    UserManager rollback

--- src/main/java/com/megadev/afterrome/manager/UserManager.java
+++ src/main/java/com/megadev/afterrome/manager/UserManager.java
@@ -3,8 +3,6 @@ package com.megadev.afterrome.manager;
 import com.megadev.afterrome.config.ConfigManager;
 import com.megadev.afterrome.config.user.ConfigUserManager;
 import com.megadev.afterrome.object.menu.shop.upgrade.skill.Skill;
-import com.megadev.afterrome.object.profession.*;
-import com.megadev.afterrome.object.skill.XSkill;
 import com.megadev.afterrome.object.user.AfterRomeUser;
 import com.megadev.afterrome.object.user.User;
 
@@ -29,37 +27,9 @@ public class UserManager extends Manager {
 
     public void serializeUser(User user) {
         List<Skill> skillList = user.getProfession().getSkills();
-        List<XSkill> xSkills = new ArrayList<>();
-        skillList.forEach(skill -> xSkills.add(new XSkill(skill.getSkillType(), skill.getLevel())));
 
-        XProfession xProfession = new XProfession(user.getProfession().getProfessionType(), xSkills);
 
-        AfterRomeUser afterRomeUser = (AfterRomeUser) user;
-        XUser xUser = new XUser(user.getUuid().toString(), xProfession, afterRomeUser.getHealths(), afterRomeUser.getPoints());
-
-        JsonSerializer.serialize(new File("data/" + user.getUuid().toString()), xUser);
-    }
-
-    public Profession getProfessionOf(XProfession xProfession) {
-//        List<Skill> skillList = new ArrayList<>();
-//        xProfession.getSkill().forEach(xSkill -> skillList.add(new S));
-
-        ///FUCKKKKKKKKKKKKKKKKKKKKKKKKKK
-        //I don't want to implement that's all...................
-
-        return switch (xProfession.getProfessionType()) {
-            case DEFAULT -> new DefaultProfession();
-            case AESCULAPIUS -> new Aesculapius();
-            case AGRONOMIST -> new Agronomist();
-            case ARTISAN -> new Artisan();
-            case ENGINEER -> new Engineer();
-            case FORESTER -> new Forester();
-            case INITIATED -> new Initiated();
-            case MAFIA -> new Mafia();
-            case SANTA -> new Santa();
-            case SHERIFF -> new Sheriff();
-            case SON_OF_MARS -> new SonOfMars();
-        };
+        JsonSerializer.serialize(new File("data/" + user.getUuid().toString()), user);
     }
 
     public void loadUsers() {
@@ -67,20 +37,8 @@ public class UserManager extends Manager {
         ConfigUserManager configUserManager = configManager.getConfig(ConfigUserManager.class);
 
         for (Player player : Bukkit.getOnlinePlayers()) {
-            if (configUserManager.userExist(player.getUniqueId())) {
-                XUser xUser = (XUser) JsonSerializer.deserialize(new File("data/" + player.getUniqueId()), XUser.class);
-
-                if (xUser == null) {
-                    saveUser(player);
-                } else {
-                    //TODO: Fuck, do it, fucking implement your SHITCODE
-//                    saveUser(new AfterRomeUser(UUID.fromString(xUser.getUuid()), xUser.getProfession(), xUser.getHealths(), xUser.getPoints()));
-                }
-
-
-            } else {
+            if (configUserManager.userExist(player.getUniqueId()))
                 saveUser(player);
-            }
         }
     }
 
